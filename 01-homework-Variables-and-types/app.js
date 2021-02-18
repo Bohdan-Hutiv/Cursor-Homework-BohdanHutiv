f3e8b37246cbc94c623b7062620582064992ecd5
Bohdan Hutiv <bestns123@gmail.com>
2021-02-18 21:55:35 +0200

    CutCUTCutCUT

--- 01-homework-Variables-and-types/app.js
+++ 01-homework-Variables-and-types/app.js
@@ -6,24 +6,29 @@ const sumPricesAllFloor =
   Math.floor(penPrice) +
   Math.floor(pineApplePrice) +
   Math.floor(applePrice);
-const buyerHas500Rest = 500 - sumPricesAll;
-const discount = getRandomInt(0, 100);
-
+const banknote500 = 500;
+const restClient = banknote500 - sumPricesAll;
+const minDiscount = 0;
+const maxDiscount = 100;
+const discount = Math.floor(
+  Math.random() * (maxDiscount - minDiscount + 1) ) + minDiscount;
+const maxPrice = Math.max(penPrice, pineApplePrice, applePrice);
+const minPrice = Math.min( penPrice, pineApplePrice, applePrice);
+const sumOfAllGoods = Math.round( (sumPricesAll) / 100 ) * 100;
+const isEvenSumOfAllGoods = sumPricesAllFloor % 2 === 0;
+const averagePriceOfAllGoods = +( sumPricesAll / 3 ).toFixed(2);
+const incomeFrombuyingAllGoods = 
+  +( sumPricesAll / 2 - (sumPricesAll * discount/100) ).toFixed(2) ;
 
 console.log(`
-  Maximum price : ${Math.max(penPrice, pineApplePrice, applePrice)}
-  Minimum price : ${Math.min( penPrice, pineApplePrice, applePrice)}
+  Maximum price : ${maxPrice}
+  Minimum price : ${minPrice}
   The sum of all goods ${sumPricesAll}
   The sum of all goods without coins ${sumPricesAllFloor}
-  The sum of all goods is rounded to the nearest hundred ${ Math.round( (penPrice + pineApplePrice + applePrice) / 100 ) * 100 }
-  Is the even sum of all goods (rounded down) : ${sumPricesAllFloor % 2 === 0}
-  The rest when paying 500 ${buyerHas500Rest}
-  The average price of goods ${(sumPricesAll / 3).toFixed(2) } 
+  The sum of all goods is rounded to the nearest hundred ${sumOfAllGoods}
+  Is the even sum of all goods (rounded down) : ${isEvenSumOfAllGoods}
+  The rest when paying 500 ${restClient}
+  The average price of goods ${averagePriceOfAllGoods} 
   Random discount = ${discount}, 
-  Net income from buying a pineApple, apple and pen ${ ( sumPricesAll/2 - (sumPricesAll * discount/100) ).toFixed(2) } `);
-
-function getRandomInt(min, max){
-  Math.ceil(min);
-  Math.floor(max);
-  return Math.floor( Math.random() * (max - min + 1) ) + min ;
-}
+  Net income from buying a pineApple, apple and pen ${incomeFrombuyingAllGoods}
+`);
